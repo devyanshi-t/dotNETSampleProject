9505f4605320a425ffe0908f94946107871cee32
Devyanshi Tiwari <devyanshi12aug@gmail.com>
2021-06-03 00:51:18 +0530

    first commit

---
+++ C# Tutorial/src/Gradebook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Gradebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           if(args.Length >0)
+           { Console.WriteLine($"Hello {args[0] }!");// String interpolation
+           }
+           else
+           {
+               Console.WriteLine("Hello World");
+           }
+        }
+    }
+}
